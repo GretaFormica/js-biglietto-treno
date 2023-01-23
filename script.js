724b8313ba6e26053243dcc309441185c1a74eba
GretaFormica <greta.formica@gmail.com>
2023-01-23 15:35:50 +0100

    arrotondamento

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // dichiarazione costanti e variabili
 const Km = parseInt(prompt('Quanti kilometri devi percorrere?'));
 const Anni = parseInt(prompt('Quanti anni hai?'));
-let PrezzoBase = km * 0.21;
+const PrezzoBase = km * 0.21;
+let Prezzo
 
 //istruzioni condizionali
 if(Anni < 18){
@@ -15,5 +16,8 @@ if(Anni < 18){
 } else {
 
     Prezzo = PrezzoBase;
-    
+
 }
+
+//arrotondamento
+Prezzo = Prezzo.toFixed(2);
