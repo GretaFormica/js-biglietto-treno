7074ee91c47761ece6ba1d8798371526bc065843
GretaFormica <greta.formica@gmail.com>
2023-01-23 15:44:18 +0100

    fix

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // dichiarazione costanti e variabili
 const Km = parseInt(prompt('Quanti kilometri devi percorrere?'));
 const Anni = parseInt(prompt('Quanti anni hai?'));
-const PrezzoBase = km * 0.21;
+const PrezzoBase = Km * 0.21;
 let Prezzo
 
 //istruzioni condizionali
 if(Anni < 18){
 
-    Prezzo = (PrezzoBase * 20) / 100;
+    Prezzo = (PrezzoBase * 20 / 100);
 
 } else if(Anni > 65){
     
-    Prezzo = (PrezzoBase * 40) / 100;
+    Prezzo = (PrezzoBase * 40 / 100);
 
 } else {
 
@@ -21,7 +21,8 @@ if(Anni < 18){
 
 //arrotondamento
 Prezzo = Prezzo.toFixed(2);
+console.log(Prezzo);
 
 //output
 
-document.getElementById('h1').innerHTML= `Il prezzo totale per il tuo viaggio è di ${Prezzo} euro`
+document.getElementById('h1').innerHTML= `Il prezzo totale per il tuo viaggio è di ${Prezzo} euro`;
